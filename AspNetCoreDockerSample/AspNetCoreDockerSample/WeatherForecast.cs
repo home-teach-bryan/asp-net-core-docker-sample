6ccac0b15fe781ca113aea733fb746ac1e37d1f6
d58526 <d58526@gmail.com>
2024-08-19 17:21:27 +0800

    Add Project and Dockerfile

---
+++ AspNetCoreDockerSample/AspNetCoreDockerSample/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace AspNetCoreDockerSample;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
